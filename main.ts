75548b52212b07e45b3c3e88a7eb9fdd483195bc
Jon Love <jon_w-l@hotmail.com>
2024-12-02 23:05:07 +0800

    Comment out postgres connection string

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 import app from './src/app.ts'
 import config from './src/config/config.ts'
 import logger from './src/utils/logger.ts'
-import { connectToDatabase } from './src/db/db.ts'
+//import { connectToDatabase } from './src/db/db.ts'
 
-const start = async () => {
-  await connectToDatabase()
+const start =  () => {
+ // await connectToDatabase()
   app.listen(config.PORT, () => {
     logger.info(`Server running on port ${config.PORT}`)
   })
